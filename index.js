502a4a185da918f6d0d4b0f591ac99a1d394c879
Mark Hellmer <mchellmer@gmail.com>
2022-07-24 17:23:04 +0100

    log token

--- index.js
+++ index.js
@@ -3,19 +3,17 @@ const { Client, GatewayIntentBits } = require('discord.js');
 
 const TOKEN = process.env.CLIENT_TOKEN
 
-console.log(`Token ${TOKEN}`)
+//Setup client to look for servers and messages 'guilds'
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages
+    ]
+})
 
-// //Setup client to look for servers and messages 'guilds'
-// const client = new Client({
-//     intents: [
-//         GatewayIntentBits.Guilds,
-//         GatewayIntentBits.GuildMessages
-//     ]
-// })
+//On successful login do something
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
 
-// //On successful login do something
-// client.on("ready", () => {
-//     console.log(`Logged in as ${client.user.tag}`)
-// })
-
-// client.login(TOKEN)
+client.login(TOKEN)
