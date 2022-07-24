5f78aec23b07327e53aed096cc9ccaeeaee2bc9d
Mark Hellmer <mchellmer@gmail.com>
2022-07-24 16:48:05 +0100

    update intent calls to latest

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const Discord = require("discord.js")
+const { Client, GatewayIntentBits } = require('discord.js');
 
 const TOKEN = process.env.GUILD_ID
 
 //Setup client to look for servers and messages 'guilds'
 const client = new Discord.Client({
     intents: [
-        "GUILDS",
-        "GUILD_MESSAGES"
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages
     ]
 })
 
